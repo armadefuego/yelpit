9421f5ba97883ae021edea7f93c2a1273b9baa67
Marc Gunn <telrikk@gmail.com>
2015-07-18 02:09:32 -0400

    basic validation of users

--- public/javascripts/models/user.js
+++ public/javascripts/models/user.js
@@ -1,14 +1,27 @@
 define([
-	'backbone'
-], function (Backbone) {
+	'backbone',
+	'lodash'
+], function (Backbone, _) {
 	window.APP = window.APP || {}
 
 	APP.UserModel = (function() {
 		return Backbone.Model.extend({
 			url: '/users',
 
-			authenticate: function(username, password) {
-				
+			validate: function (attributes) {
+				var errors = {};
+				var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    			if (!pattern.test(attributes.email)) {
+    				errors.email = 'Please enter a valid email address.'
+    			}
+
+				if (attributes.password != attributes.passwordConfirmation) {
+					errors.password = 'Passwords do not match.'
+				}
+
+				if (!_.isEmpty(errors)) {
+					return errors;
+				}
 			}
 		});	
 	}());
--- public/javascripts/views/register.js
+++ public/javascripts/views/register.js
@@ -10,6 +10,7 @@ define([
 
 			initialize: function(options) {
 				this.model = options.model;
+				this.model.bind('invalid', this.showErrors, this);
 			}, 
 
 			render: function() {
@@ -17,8 +18,22 @@ define([
 				return this;
 			},
 
+			showErrors: function(user, errors) {
+			    this.$el.find('.error').removeClass('error');
+			    this.$el.find('.alert').html(_.values(errors).join('<br>')).show();
+			    // highlight the fields with errors
+			    _.each(_.keys(errors), _.bind(function (key) {
+			      this.$el.find('*[id=' + key + ']').addClass('error');
+			    }, this));
+			},
+
 			register: function(e) {
 				e.preventDefault();
+				this.model.set({
+					email: this.$el.find('input[id=email]').val(),
+					password: this.$el.find('input[id=password]').val(),
+					passwordConfirmation: this.$el.find('input[id=confirmpassword]').val()
+				});
 				this.model.save();
 			}
 		});
